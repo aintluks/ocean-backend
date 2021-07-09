2e07b57fdc39f332aa97cdf2aea036e8a3236223
Lucas da Costa <aintluks@gmail.com>
2021-07-09 14:09:22 -0300

    add: third class path to backend samsung ocean

--- index.js
+++ index.js
@@ -1,47 +1,61 @@
 const express = require("express");
+const { MongoClient, ObjectID } = require("mongodb");
+
+(async () => {
+  const url = "mongodb://localhost:27017";
+  const dbname = "ocean_bancodados_09_07_2021";
+
+  console.log("Conectando ao banco de dados...");
+
+  const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+
+  console.log("MongoDB conectado com sucesso!");
+
+  const db = client.db(dbname);
+  const games = db.collection("games");
+
+  app.get("/games", async (req, res) => {
+    const gameList = await games.find().toArray();
+
+    return res.send(gameList);
+  });
+
+  app.get("/games/:id", async (req, res) => {
+    const id = req.params.id;
+
+    const game = await games.findOne({ _id: ObjectID(id) });
+
+    return res.send(game);
+  });
+
+  app.post("/games", async (req, res) => {
+    const game = req.body;
+
+    await games.insertOne(game);
+
+    return res.send(game);
+  });
+
+  app.put("/games/:id", async (req, res) => {
+    const id = req.params.id;
+    const game = req.body;
+
+    await games.updateOne({ _id: ObjectID(id) }, { $set: game });
+
+    return res.send(game);
+  });
+
+  app.delete("/games/:id", async (req, res) => {
+    const id = req.params.id;
+
+    await games.deleteOne({ _id: ObjectID(id) });
+
+    return res.send("Game removed...");
+  });
+})();
+
 const app = express();
 
 app.use(express.json());
-
-const lista = [
-  "League of Legends",
-  "Overwatch",
-  "Teamfight Tactics",
-  "Valorant",
-];
-
-app.get("/games", (req, res) => {
-  res.send(lista.filter(Boolean));
-});
-
-app.get("/games/:id", (req, res) => {
-  const id = req.params.id;
-
-  res.send(lista[id]);
-});
-
-app.post("/games", (req, res) => {
-  const game = req.body.game;
-
-  lista.push(game);
-
-  res.send(lista);
-});
-
-app.put("/games/:id", (req, res) => {
-  const id = req.params.id;
-
-  lista[id] = req.body.game;
-
-  res.send(lista[id]);
-});
-
-app.delete("/games/:id", (req, res) => {
-  const id = req.params.id;
-
-  delete lista[id];
-
-  res.send(lista.filter(Boolean));
-});
 
 app.listen(3000);
---
+++ static.js
@@ -0,0 +1,42 @@
+const app;
+
+const lista = [
+  "League of Legends",
+  "Overwatch",
+  "Teamfight Tactics",
+  "Valorant",
+];
+
+app.get("/games", (req, res) => {
+  res.send(lista.filter(Boolean));
+});
+
+app.get("/games/:id", (req, res) => {
+  const id = req.params.id;
+
+  res.send(lista[id]);
+});
+
+app.post("/games", (req, res) => {
+  const game = req.body.game;
+
+  lista.push(game);
+
+  res.send(lista);
+});
+
+app.put("/games/:id", (req, res) => {
+  const id = req.params.id;
+
+  lista[id] = req.body.game;
+
+  res.send(lista[id]);
+});
+
+app.delete("/games/:id", (req, res) => {
+  const id = req.params.id;
+
+  delete lista[id];
+
+  res.send(lista.filter(Boolean));
+});
