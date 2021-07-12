87ec04b03be6b7b4e7c1e3d8a55bd843c6351590
Lucas da Costa <aintluks@gmail.com>
2021-07-12 10:55:53 -0300

    fix to deploy

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const { MongoClient, ObjectID } = require("mongodb");
 
   console.log("Conectando ao banco de dados...");
 
-  const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+  // const client = await MongoClient.connect(url, { useUnifiedTopology: true });
 
   console.log("MongoDB conectado com sucesso!");
 
-  const db = client.db(dbname);
-  const games = db.collection("games");
+  // const db = client.db(dbname);
+  // const games = db.collection("games");
+  const games = undefined;
 
   app.get("/", (req, res) => {
     return res.send("He ll o");
@@ -22,7 +23,8 @@ const { MongoClient, ObjectID } = require("mongodb");
     const gameList = await games.find().toArray();
 
     return res.send(gameList);
-  });
+  https://aintluks-ocean-backend.herokuapp.com/
+});
 
   app.get("/games/:id", async (req, res) => {
     const id = req.params.id;
